0f31fce09427abba27a1a0b9e8645321072fa267
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-02-27 04:28:30 +0530

    Question added in basic

---
+++ 1 Basics/1.7 AreaOfSqr.py
@@ -0,0 +1,15 @@
+# Q. WAP to find area of square
+# Side * side = side ** 2
+
+side = int(input("Enter side of square : "))
+area = side * side 
+print("Area of square is : ",area)
+
+# Q. WAP to input 2 floating point numbers & print their average
+
+num1 = float(input("Enter Num1 : "))
+num2 = float(input("Enter Num2 : "))
+
+avg = (num1 + num2) / 2
+
+print("Average : ", avg)
