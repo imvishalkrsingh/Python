cab8243fc335475e0831fcd64dcc42026c8753db
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-02-27 04:37:08 +0530

    Question of conditional

---
+++ 2 Control flow/2.5 Question.py
@@ -0,0 +1,14 @@
+# WAP to input 2 int number a and b 
+# print true if a is greater than or equal to b. if not print false
+
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b : "))
+
+if(a >= b):
+    print("True")
+
+else:
+    print("False")
+
+
+# 
