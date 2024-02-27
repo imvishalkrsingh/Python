ab2dba6c1e9bf8c70dbc5b0ef721b9a37353ebe8
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-02-27 05:40:15 +0530

    Question added

---
+++ 2 Control flow/2.6 Greatest.py
@@ -0,0 +1,14 @@
+# WAP to find the greatest of 3 numbers entered by the user.
+
+num1 = 4
+num2 = 5
+num3 = 6
+
+if(num1 > num2 and num1 > num3):
+    print("num1 is greater", num1)
+
+elif(num2 > num3):
+    print("num2 is greater", num2)
+else :
+    print("num3 is greater", num3)
+
